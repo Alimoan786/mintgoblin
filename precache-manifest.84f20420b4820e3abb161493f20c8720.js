ca8d4000bc293b9ec3462a2aafd00f0fde1f0ae4
Alimoan786 <peoplechooser@gmail.com>
2022-08-26 10:57:24 +0530

    Added foo to the bar

---
+++ precache-manifest.84f20420b4820e3abb161493f20c8720.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1861753fae6be458ab09",
+    "url": "/static/css/main.88d35380.chunk.css"
+  },
+  {
+    "revision": "1861753fae6be458ab09",
+    "url": "/static/js/main.1861753f.chunk.js"
+  },
+  {
+    "revision": "1f52d3e5ac4c3cc2dcbf",
+    "url": "/static/js/1.1f52d3e5.chunk.js"
+  },
+  {
+    "revision": "bf9fce4e250d23cef84c",
+    "url": "/static/js/2.bf9fce4e.chunk.js"
+  },
+  {
+    "revision": "dcdad6cff46524f5966d",
+    "url": "/static/js/runtime~main.dcdad6cf.js"
+  },
+  {
+    "revision": "e63562614cbdc491e4d374f97c513b82",
+    "url": "/static/media/font.e6356261.woff"
+  },
+  {
+    "revision": "6403a66a9e248f25b9a991326c1c737e",
+    "url": "/index.html"
+  }
+];
